4d6daaa80c8c65da730582186e9156a566a60f7c
Anirudh Acharya <anirudhra@users.noreply.github.com>
2016-10-14 14:29:07 -0700

    Adding shell init files

---
+++ brew/brew_apps_install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#
+# command line apps
+#
+echo "Installing command line apps..."
+brew tap homebrew/dupes
+brew install wget
+brew install homebrew/dupes/tidy
+brew install node
+brew install imagemagick
+brew install ack
+brew install fortune
+brew install cowsay
+brew install colordiff
+brew install gawk
+brew install gnu-sed --with-default-names
+brew install p7zip
+brew install pv
+brew install sshfs
+brew install htop
+brew install calc
+brew install dos2unix
+brew install ext2fuse
+brew install ext4fuse
+brew install homebrew/dupes/grep
+brew install nmap
+brew install ntfs-3g
+brew install rename
+brew install watch
+brew install tree
+brew install trash
+brew install imagemagick
+echo "Installing command line apps... done!"
+
+#
+# cask gui apps
+#
+echo "Installing GUI apps..."
+brew cask install textexpander
+brew cask install macvim
+brew cask install alfred
+brew cask install gimp
+brew cask install xquartz
+brew cask install osxfuse
+echo "Installing GUI apps... done!"
+
+echo "Cleaning up..."
+brew cleanup
+brew cask cleanup
+echo "Cleaning up... done!"
+
---
+++ vim/README.md
@@ -0,0 +1,7 @@
+# vimfiles
+
+Vim configuration
+
+Windows: put these in /Program Files/vim directory
+UNIX/Linux: put these in ~/.vim directory; move 'colors' directory to ~/.vim with no vimfiles directory
+'
